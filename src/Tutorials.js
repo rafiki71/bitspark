8aa8ff4ef8fa0965593d30211ba00033300990ff
Rafiki the Wise <rafiki@bitsperity.dev>
2023-07-07 19:13:11 +0000

    First tutorial is now complete

--- src/Tutorials.js
+++ src/Tutorials.js
@@ -1,9 +1,29 @@
 const tutorials = [
     {
-        bannerImage: "../../img/Banner1u.png",
+        bannerImage: "../../img/tutorial/nostr_banner.png",
         title: "What is Nostr?",
-        subtitle: "This is a subtitle for tutorial 1",
-        content: "This is the content for tutorial 1."
+        subtitle: "Nostr - The decentralized State",
+        content: `
+        <div style="font-family: Arial, sans-serif; padding: 20px;">
+            <h1>Welcome to the world of Nostr!</h1>
+            <p>Perhaps you've heard about Nostr, the protocol that's sparking conversations and stretching imaginations all across the digital world. Well, we're here to guide you through it and make understanding and using Nostr as easy as possible. Let's dive in!</p>
+    
+            <h2>So, what is Nostr?</h2>
+            <p>Nostr is a decentralized messaging protocol. But don't worry if this sounds too technical! Simply put, Nostr allows you to send messages on the internet in a whole new way - free from central control.</p>
+    
+            <h2>What makes Nostr unique?</h2>
+            <p>Every user in Nostr has a public key, which is like your address that everyone can see, and a private key, which acts like your secret password. Be sure to keep your private key safe because just like a house key, it cannot be replaced if lost!</p>
+            <p>What's even more exciting is that with Nostr, your account can act as a universal key! Imagine being able to log in to multiple platforms and websites with just a single account. Want to post on a social media platform? Check. Need to log in to your online workspace? Done. Want to comment on your favorite blog? Easy peasy! All these and many more are possible with your Nostr account.</p>
+    
+            <h2>But, how does it work?</h2>
+            <p>You can access Nostr through an app on your computer, your smartphone, or directly in your web browser. These apps allow you to send and receive messages. And the best part? Each message you send is signed with your private key, assuring everyone that the message is genuinely yours. It's like your personal signature on every message you send.</p>
+    
+            <h2>The magic of Nostr</h2>
+            <p>Now, you might wonder why Nostr seems to be stirring up so much buzz. The magic of Nostr lies in its decentralization. Unlike traditional systems where there's a central authority, Nostr is spread out. This means there's no single entity controlling your messages. You're in control of your own communication. And that's truly magical.</p>
+    
+            <h2>Let's get started!</h2>
+            <p>Now that you've gotten a glimpse of the amazing world of Nostr, you're probably eager to jump in. And we're here to guide you every step of the way. Stay tuned for our next tutorial, where we'll walk you through setting up your first Nostr app. The future of communication awaits!</p>
+        </div>`
     },
     {
         bannerImage: "../../img/Banner1u.png",
