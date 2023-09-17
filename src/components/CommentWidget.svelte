c51c4a73739cb39851574f989eba3e3270e96468
Rafiki the Wise <rafiki@bitsperity.dev>
2023-09-17 14:23:26 +0000

    CommentWidget is now in its own file.

---
+++ src/components/CommentWidget.svelte
@@ -0,0 +1,92 @@
+<!-- CommentWidget.svelte -->
+
+<script>
+  import { helperStore } from "../helperStore.js";
+  import ProfileImg from "../components/ProfileImg.svelte";
+
+  export let id;
+
+  let comments = [];
+  let newComment = "";
+  let profiles = {};
+
+  async function fetchComments() {
+    try {
+      const fetchedComments = await $helperStore.getComments(id);
+      comments = await Promise.all(
+        fetchedComments.map(async (comment) => {
+          const profile = await $helperStore.getProfile(comment.pubkey);
+          profiles[comment.pubkey] = profile;
+          return {
+            id: comment.id,
+            comment: comment.content,
+            name: comment.name,
+            picture: profile.picture,
+            pubkey: comment.pubkey,
+            githubVerified: profile.githubVerified,
+          };
+        })
+      );
+    } catch (error) {
+      console.error("Error fetching comments data:", error);
+    }
+  }
+
+  async function submitComment() {
+    if (newComment.trim() === "") return;
+
+    try {
+      const commentId = await $helperStore.postComment(id, newComment);
+      await fetchComments();
+      newComment = "";
+    } catch (error) {
+      console.error("Error submitting comment:", error);
+    }
+  }
+
+  $: fetchComments(), $helperStore;
+</script>
+
+<div class="container bg-card p-4">
+  <h4 class="base-h4">Comments</h4>
+  <ul>
+    {#each comments as comment (comment.id)}
+      <li class="flex items-center gap-4 my-2">
+        {#if comment.picture}
+          <div style="margin-right: 10px;">
+            <ProfileImg
+              profile={comment}
+              style={{ width: "40px", height: "40px" }}
+            />
+          </div>
+        {/if}
+        <div>
+          <h3 class="font-bold text-sm">{comment.name}</h3>
+          <p class="text-m">{comment.comment}</p>
+        </div>
+      </li>
+    {/each}
+  </ul>
+  <div class="mt-6">
+    <label for="newComment" class="text-lg text-blueGray-600">Your Comment:</label>
+    <textarea
+      id="newComment"
+      class="w-full h-24 p-2 mt-2 rounded-md border-2 border-blueGray-200"
+      bind:value={newComment}
+      placeholder="Schreibe hier deinen Kommentar..."
+    />
+    <div style="text-align: right;">
+      <button
+        class="bg-orange-500 active:bg-orange-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none mt-4 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        on:click={submitComment}
+      >
+        Send
+      </button>
+    </div>
+  </div>
+</div>
+
+<style>
+  /* Deine Styles hier (falls benötigt) */
+</style>
--- src/views/IdeaDetail.svelte
+++ src/views/IdeaDetail.svelte
@@ -5,6 +5,7 @@
   import { sendSatsLNurl } from "../LNHelper.js";
   import Menu from "../components/Menu.svelte";
   import ProfileImg from "../components/ProfileImg.svelte";
+  import CommentWidget from "../components/CommentWidget.svelte";
   import Footer from "../components/Footers/FooterBS.svelte";
   import { helperStore } from "../helperStore.js"; // Import the store
   import { sidebarOpen } from "../helperStore.js";
@@ -22,8 +23,6 @@
     subtitle: "Idea Engine",
   };
 
-  let comments = [];
-  let newComment = "";
   let profiles = {};
   let creator_profile = null;
 
@@ -40,7 +39,6 @@
 
   onMount(async () => {
     await fetchData();
-    await fetchComments();
   });
 
   async function fetchData() {
@@ -76,39 +74,6 @@
     await sendSatsLNurl(idea.lnAdress);
   }
 
-  async function fetchComments() {
-    try {
-      const fetchedComments = await $helperStore.getComments(id);
-      comments = await Promise.all(
-        fetchedComments.map(async (comment) => {
-          const profile = await $helperStore.getProfile(comment.pubkey);
-          profiles[comment.pubkey] = profile;
-          return {
-            id: comment.id,
-            comment: comment.content,
-            name: comment.name,
-            picture: profile.picture, // Benutze das geladene Profilbild
-            pubkey: comment.pubkey,
-            githubVerified: profile.githubVerified,
-          };
-        })
-      );
-    } catch (error) {
-      console.error("Error fetching comments data:", error);
-    }
-  }
-
-  async function submitComment() {
-    if (newComment.trim() === "") return;
-
-    try {
-      const commentId = await $helperStore.postComment(id, newComment);
-      await fetchComments();
-      newComment = "";
-    } catch (error) {
-      console.error("Error submitting comment:", error);
-    }
-  }
   let contentContainerClass = "combined-content-container";
 
   $: {
@@ -119,7 +84,6 @@
     }
   }
   $: fetchData(), $helperStore;
-  $: fetchComments(), $helperStore;
 </script>
 
 <main class="overview-page">
@@ -166,48 +130,7 @@
           </div>
         </div>
       </div>
-
-      <div class="container bg-card p-4">
-        <h4 class="base-h4">Comments</h4>
-        <ul>
-          {#each comments as comment (comment.id)}
-            <li class="flex items-center gap-4 my-2">
-              {#if comment.picture}
-                <div style="margin-right: 10px;">
-                  <ProfileImg
-                    profile={comment}
-                    style={{ width: "40px", height: "40px" }}
-                  />
-                </div>
-              {/if}
-              <div>
-                <h3 class="font-bold text-sm">{comment.name}</h3>
-                <p class="text-m">{comment.comment}</p>
-              </div>
-            </li>
-          {/each}
-        </ul>
-        <div class="mt-6">
-          <label for="newComment" class="text-lg text-blueGray-600"
-            >Your Comment:</label
-          >
-          <textarea
-            id="newComment"
-            class="w-full h-24 p-2 mt-2 rounded-md border-2 border-blueGray-200"
-            bind:value={newComment}
-            placeholder="Schreibe hier deinen Kommentar..."
-          />
-          <div style="text-align: right;">
-            <button
-              class="bg-orange-500 active:bg-orange-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none mt-4 mb-1 ease-linear transition-all duration-150"
-              type="button"
-              on:click={submitComment}
-            >
-              Send
-            </button>
-          </div>
-        </div>
-      </div>
+      <CommentWidget id={id} />
     </div>
     <Footer />
   </div>
