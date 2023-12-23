1d5d7abbfb050d94716494121269f1776fc21cd2
rafiki71 <rafiki71@bitsperity.dev>
2023-12-23 13:32:14 +0100

    OfferBubble is now complete. but design needs to be improved.

--- src/components/JobManager2/OfferBubble.svelte
+++ src/components/JobManager2/OfferBubble.svelte
@@ -1,6 +1,9 @@
 <script>
     import BaseBubble from "./BaseBubble.svelte";
-    
+    import { onMount } from "svelte";
+    import { nostrCache } from "../../backend/NostrCacheStore.js";
+    import { nostrManager } from "../../backend/NostrManagerStore.js";
+
     export let event;
 
     let sats = 0;
@@ -8,6 +11,85 @@
     let backgroundColor = "#E8F4FA"; // Ein sanftes Blau
     let textColor = "#333333"; // Dunkelgrau für guten Kontrast
     let satsColor = "#4A90E2"; // Hervorstechendes Blau für Sats
+    let acceptButtonColor = "#4CAF50"; // Grün für Akzeptieren
+    let declineButtonColor = "#F44336"; // Rot für Ablehnen
+    let statusTextColor = "#333333"; // Dunkelgrau für normalen Status
+    let isJobCreator = false;
+    let offerStatus = "pending"; // "accepted", "declined", "pending"
+
+    async function checkOfferStatus() {
+        offerStatus = "pending";
+        const responses = $nostrCache.getEventsByCriteria({
+            kinds: [1337],
+            tags: {
+                o: [event.id],
+                t: ["ao", "do"],
+                s: ["bitspark"],
+            },
+        });
+
+        console.log("responses:", responses);
+        
+        if (responses.length > 0) {
+            offerStatus =
+                responses[0].tags.find((tag) => tag[0] === "t")[1] === "ao"
+                    ? "accepted"
+                    : "declined";
+        }
+        console.log("offerStatus:", offerStatus);
+    }
+
+    async function handleAccept() {
+        await postResponse("ao", "accepted");
+    }
+
+    async function handleDecline() {
+        await postResponse("do", "declined");
+    }
+
+    async function postResponse(responseType, content) {
+        if (!$nostrManager || !$nostrManager.write_mode) return;
+
+        const jobId = event.tags.find((tag) => tag[0] === "e")?.[1];
+        const offerId = event.id;
+        const tags = [
+            ["t", responseType],
+            ["e", jobId],
+            ["o", offerId],
+            ["s", "bitspark"],
+        ];
+
+        try {
+            await $nostrManager.sendEvent(1337, content, tags);
+            console.log("Response sent successfully");
+        } catch (error) {
+            console.error("Error sending response:", error);
+        }
+    }
+
+    onMount(async () => {
+        await checkIfJobCreator();
+        await checkOfferStatus();
+    });
+
+    async function checkIfJobCreator() {
+        const jobId = event.tags.find((tag) => tag[0] === "e")?.[1];
+        if (jobId) {
+            const jobEvents = $nostrCache.getEventsByCriteria({
+                kinds: [1337], // Annahme, dass 1337 das Kind für Job-Postings ist
+                ids: [jobId],
+                tags: {
+                    s: ["bitspark"],
+                },
+            });
+
+            if (jobEvents.length > 0) {
+                const jobEvent = jobEvents[0];
+                isJobCreator = jobEvent.pubkey === $nostrManager.publicKey;
+                console.log("isJobCreator:", isJobCreator);
+            }
+        }
+    }
 
     // Reaktive Anweisungen für die Event-Daten
     $: if (event && event.tags) {
@@ -18,12 +100,35 @@
     $: if (event) {
         offerMsg = event.content || "Keine Beschreibung verfügbar.";
     }
+
+    // Reaktive Anweisungen
+    $: $nostrCache && checkOfferStatus();
+    //$: $nostrManager && initialize();
 </script>
 
-<BaseBubble event={event} {backgroundColor} {textColor}>
+<BaseBubble {event} {backgroundColor} {textColor}>
     <div class="offer-content">
         <h3 class="sats-amount" style="color: {satsColor};">{sats} Sats</h3>
         <p class="offer-msg">{offerMsg}</p>
+        {#if isJobCreator && offerStatus === "pending"}
+            <div class="offer-actions">
+                <button
+                    on:click={handleAccept}
+                    style="background-color: {acceptButtonColor};"
+                    >Akzeptieren</button
+                >
+                <button
+                    on:click={handleDecline}
+                    style="background-color: {declineButtonColor};"
+                    >Ablehnen</button
+                >
+            </div>
+        {/if}
+        {#if offerStatus !== "pending"}
+            <div class="offer-status" style="color: {statusTextColor};">
+                {offerStatus === "accepted" ? "Akzeptiert" : "Abgelehnt"}
+            </div>
+        {/if}
     </div>
 </BaseBubble>
 
@@ -43,5 +148,22 @@
     .offer-msg {
         margin-top: 0;
         line-height: 1.4;
+    }
+
+    .offer-actions button {
+        padding: 5px 10px;
+        border: none;
+        color: white;
+        border-radius: 4px;
+        margin-right: 10px;
+        cursor: pointer;
+        font-weight: bold;
+    }
+
+    .offer-status {
+        padding: 5px;
+        border-radius: 4px;
+        font-weight: bold;
+        margin-top: 10px;
     }
 </style>
